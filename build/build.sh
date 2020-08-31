e1774a6d00933ef24b0c64b716e1e6c5307ef009
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-08-31 17:31:30 +0900

    update Dockerfile

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,5 @@
 # Please use from top of project directory.
 # bash build/build.sh
 
-# go build
-docker build -t build-swaggo-test:0.1 -f build/Dockerfile.build
-
 # docker build
 docker build -t swaggo-test:0.1 -f build/Dockerfile
