89fc6c46bc3c67860a611ff68ca8bb93831eed5f
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-08-31 21:17:23 +0900

    update docker-compose deploy script

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -1,6 +1,8 @@
 # Please use from top of project directory.
 # bash deployments/deploy.sh
 
-docker-compose -f deployments/docker-compose.yml up -d
-docker-compose -f deployments/docker-compose.yml exec db \
-  mysql -u root -p root -h localhost < ${PWD}/deployments/initialize_db.sh
+rm -rf deployments/mysql/data
+mkdir deployments/mysql/data
+
+# docker-compose -f deployments/docker-compose.yml up -d
+podman-compose -f deployments/docker-compose.yml up -d
--- deployments/mysql/sql/initialize_db.sql
+++ deployments/mysql/sql/initialize_db.sql
@@ -1,5 +1,3 @@
-DROP DATABASE practice;
-
 CREATE DATABASE practice;
 USE practice;
 
@@ -18,7 +16,7 @@ PRIMARY KEY (uuid) );
 INSERT INTO practice.instances (uuid, name, owner)
   VALUES ("cde11bfa-e973-11ea-9c6e-0a15bbaed69b", "web01-2020-08-29", "bd64e65e-e972-11ea-842a-0a15bbaed69b");
 INSERT INTO practice.instances (uuid, name, owner)
-  VALUES ("b38f005a-e973-11ea-b90d-0a15bbaed69b", "mysql01-2020-08-29", "fba89220-4eb3-4c6e-9990-f2dea16fb928");
+  VALUES ("b38f005a-e973-11ea-b90d-0a15bbaed69b", "mysql01-2020-08-29", "bd64e65e-e972-11ea-842a-0a15bbaed69b");
 
 INSERT INTO practice.images (uuid, name, owner)
   VALUES ("cde11bfa-e973-11ea-9c6e-0a15bbaed69b", "web01-2020-08-29", "bd64e65e-e972-11ea-842a-0a15bbaed69b");
