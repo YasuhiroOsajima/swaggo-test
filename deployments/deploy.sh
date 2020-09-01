85b6888bbdcdab2fbbecf91e8c7b7e32e708f253
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-09-01 10:24:10 +0000

    bug fix in deploy script

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -4,5 +4,9 @@
 rm -rf deployments/mysql/data
 mkdir deployments/mysql/data
 
+sed -i '/^SERVER=/d' deployments/server.env
+IPADDRESS=`hostname -i | awk '{print $4}'`
+echo "SERVER=${IPADDRESS}" >> deployments/server.env
+
 # docker-compose -f deployments/docker-compose.yml up -d
 podman-compose -f deployments/docker-compose.yml up -d
