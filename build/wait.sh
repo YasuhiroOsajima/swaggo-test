f179641c4e4b581ea1a21360bc761a49ffc7deb9
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-09-01 13:43:47 +0000

    update wait script

--- build/wait.sh
+++ build/wait.sh
@@ -3,7 +3,13 @@
 BIN_PATH=/swaggo-test/build/server
 MYSQL_SERVER=mysql_host
 
-until mysqladmin ping -h ${MYSQL_SERVER} --silent; do
+for ((i=0;i<5;i++)); do
+  mysqladmin ping -h ${MYSQL_SERVER} --silent
+  if [ $? -eq 0 ]; then
+    echo 'mysql server became connectable.'
+    break
+  fi
+
   echo 'waiting for mysql server becomes connectable.'
   sleep 2
 done
