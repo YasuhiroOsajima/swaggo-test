e626b6cba476679ae25b6ffe85c14db0b212d315
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-09-01 09:06:49 +0000

    add loop try to connect mysql

--- internal/repository/connect.go
+++ internal/repository/connect.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"log"
 	"os"
+	"strings"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -13,9 +15,22 @@ var db *sqlx.DB
 func init() {
 	server := os.Getenv("DBSERVER")
 	port := os.Getenv("DBPORT")
+	user := os.Getenv("DBUSER")
+	password := os.Getenv("DBPASS")
 
 	var err error
-	db, err = sqlx.Connect("mysql", "root:root@tcp("+server+":"+port+")/practice")
+	waitMsg := "connect: connection refused"
+	notInclude := -1
+
+	for i := 0; i < 5; i++ {
+		db, err = sqlx.Connect("mysql", user+":"+password+"@tcp("+server+":"+port+")/practice")
+		if strings.Index(err.Error(), waitMsg) == notInclude {
+			log.Fatalf("DB connect failed. err: %s", err)
+			time.Sleep(time.Second * 10)
+			continue
+		}
+		break
+	}
 	if err != nil {
 		log.Fatalf("DB connect failed. err: %s", err)
 		panic(err)
