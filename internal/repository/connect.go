99887a13383a9383825f62bc42e755d46f2d4c3c
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-09-01 10:33:07 +0000

    remove loop connect to db

--- internal/repository/connect.go
+++ internal/repository/connect.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"log"
 	"os"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -18,15 +17,7 @@ func init() {
 	password := os.Getenv("DBPASS")
 
 	var err error
-
-	for i := 0; i < 5; i++ {
-		db, err = sqlx.Connect("mysql", user+":"+password+"@tcp("+server+":"+port+")/practice")
-		if err != nil {
-			log.Fatalf("DB connect failed. Retry after 10 sec. err: %s", err)
-			time.Sleep(time.Second * 10)
-			continue
-		}
-	}
+	db, err = sqlx.Connect("mysql", user+":"+password+"@tcp("+server+":"+port+")/practice")
 	if err != nil {
 		log.Fatalf("DB connect failed. err: %s", err)
 		panic(err)
